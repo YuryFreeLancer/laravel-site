397aa0ff1b4cf803b66cc8c25b8ba76b357f6696
YuryFreeLancer <kulyasov555@gmail.com>
2025-01-23 14:52:57 +0300

    изучил 16. installed Bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
+<div class="container">
+    <div class="row">
         <nav>
             <ul>
                 <li><a href="{{ route('main.index') }}">Main</a></li>
