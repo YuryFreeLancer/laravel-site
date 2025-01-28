f57109885b98b78c49cbf7bbf096d46d51997300
Yury <kulyasov555@gmail.com>
2025-01-28 22:23:19 +0300

    Studied 18. Отношения один ко многим 09:46

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Catgoriy;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -10,8 +11,11 @@ class PostController extends Controller
     public function index(){
 
         $posts = Post::all();
+        $category = Catgoriy::find(1);
 
-        return view('post.index', compact('posts'));
+        dd($category->posts);
+
+        //return view('post.index', compact('posts'));
     }
 
     public function create(){
