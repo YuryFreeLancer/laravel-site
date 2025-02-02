350d098b908ce250e8270b30b0c8b3a65be3ef68
Yury <kulyasov555@gmail.com>
2025-02-02 13:33:14 +0300

    22. CRUD через интерфейс - модифицируем интерфейс часть 3 end.

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -28,6 +28,19 @@
 
                 </select>
             </div>
+            <div class="form-group">
+                <label for="tags">Tags</label>
+                <select multiple class="form-control" id="tags" name="tags[]">
+                    @foreach($tags as $tag)
+                        <option
+                            @foreach($post->tags as $postTag)
+                                {{ $tag->id === $postTag->id  ? ' selected' : ''}}
+                            @endforeach
+                            value="{{ $tag->id }}">{{ $tag->title }}</option>
+                    @endforeach
+                </select>
+            </div>
+
             <button type="submit" class="btn btn-primary">update</button>
         </form>
         <a href="{{ route('post.index') }}" class="btn btn-primary mb-3">Back</a>
