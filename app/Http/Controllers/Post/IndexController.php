dc2f8db8b551bec86c616697d2d041d59734c39d
YuryFreeLancer <kulyasov555@gmail.com>
2025-02-05 15:53:06 +0300

    29. Пагинация в Laravel end

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -9,7 +9,8 @@ class IndexController extends BaseController
 {
     public function __invoke()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(10);
+        //dd($posts);
         return view('post.index', compact('posts'));
     }
 
