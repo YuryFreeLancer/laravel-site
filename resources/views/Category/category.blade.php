fe9556fba22683f4e0d1cd8bfe0a7b9120acea46
YuryFreeLancer <kulyasov555@gmail.com>
2025-02-20 15:46:49 +0300

    Сделал линки

--- resources/views/Category/category.blade.php
+++ resources/views/Category/category.blade.php
@@ -1,9 +1,10 @@
 <li>
-    {{ $category->title }}
+    <a href="{{ route('category.show', $category->id) }}"> {{ $category->title }}</a>
     @if ($category->children->count())
         <ul>
             @foreach($category->children as $child)
-                @include('category.category', ['category' => $child])
+                <a href="{{ route('category.show', $category->id) }}">  @include('category.category', ['category' => $child])</a>
+
             @endforeach
         </ul>
     @endif
